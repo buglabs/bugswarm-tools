ffd0e7a623f08a5b93a40b9eaa3c1185086ecf21
Andrew Turley <andrew.turley@buglabs.net>
2011-08-24 11:55:17 -0400

    first commit

---
+++ swarmtoolscore.py
@@ -0,0 +1,10 @@
+import ConfigParser
+
+def get_keys():
+    config = ConfigParser.ConfigParser();
+    raw_data = config.read("swarm.cfg")
+    master = config.get("Settings", "master")
+    consumer = config.get("Settings", "consumer")
+    producer = config.get("Settings", "producer")
+    return {"master" : master, "consumer": consumer, "producer": producer}
+
