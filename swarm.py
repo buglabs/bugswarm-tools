e9199d3be7520962a201565ecd50036513ab6a81
Andrew Turley <andrew.turley@buglabs.net>
2011-08-25 16:13:28 -0400

    adds update_swarm

--- swarm.py
+++ swarm.py
@@ -35,6 +35,16 @@ def list_swarms(api_key):
     conn.close()
     print json.dumps(json.loads(txt), sort_keys=True, indent=4)
 
+def update_swarm(api_key, swarm_id, changes):
+    conn = httplib.HTTPConnection('api.bugswarm.net')
+    update_swarm_json = json.dumps(changes)
+    print update_swarm_json
+    conn.request("PUT", "/swarms/%s"%(swarm_id), update_swarm_json, {"x-bugswarmapikey": api_key,  "content-type": "application/json"})
+    resp = conn.getresponse()
+    txt = resp.read()
+    conn.close()
+    print txt
+
 def destroy_swarm(api_key, swarm_id):
     conn = httplib.HTTPConnection('api.bugswarm.net')
     conn.request("DELETE", "/swarms/%s"%(swarm_id), None, {"x-bugswarmapikey": api_key})
@@ -63,6 +73,19 @@ def main():
             usage_destroy(sys.argv[0])
         swarm_id = sys.argv[2]
         destroy_swarm(keys["master"], swarm_id)
+    elif sys.argv[1] == "update":
+        if len(sys.argv) < 3:
+            usage_update(sys.argv[0])
+        swarm_id = sys.argv[2]
+        changes = {}
+        if sys.argv.count("--public") > 0:
+            changes["public"] = True
+        elif sys.argv.count("--private") > 0:
+            changes["public"] = False
+        descriptions = [x for x in sys.argv[3:] if x != "--private" and x != "--public"]
+        if len(descriptions) > 0:
+            changes["description"] = descriptions[0]
+        update_swarm(keys["master"], swarm_id, changes)
     else:
         print "Command '%s' is invalid"%(sys.argv[1])
         usage(sys.argv[0])
