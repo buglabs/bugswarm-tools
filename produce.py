23f88792ed69f79013ecfabd630a1955f10a0e10
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-08-30 13:48:33 -0400

    fixes to push data

--- produce.py
+++ produce.py
@@ -3,13 +3,26 @@ import sys
 import httplib
 import os
 import time
+import signal
+import readline
+
+conn = None
 
 def usage(name):
     print "%s SWARM_ID RESOURCE_NAME FEED_NAME"%(name)
     print "Message are read from stdin, each message is one line long, messages are separated by newline characters"
     sys.exit()
 
+def signal_handler(signal, frame):
+        global conn
+        conn.close()
+
+        print 'Http connection closed.'
+        sys.exit(0)
+
+
 def produce(api_key, swarm_id, resource_name, feed_name):
+    global conn 
     conn = httplib.HTTPConnection('api.bugswarm.net')
     #conn = httplib.HTTPConnection('127.0.0.1')
     sel = "/resources/%s/feeds/%s?swarm_id=%s"%(resource_name, feed_name, swarm_id)
@@ -25,18 +38,19 @@ def produce(api_key, swarm_id, resource_name, feed_name):
     #conn.send("1\r\n")
     #time.sleep(1)
     try:
-        for msg in sys.stdin.readlines():
+        while 1: 
+            msg = raw_input()
+            #print "msg: %s"%(msg)
             stripped_msg = msg.strip()
-            size = hex(len(stripped_msg))[2:] + "\r\n"
-            chunk = stripped_msg + "\r\n"
-            conn.send(size)
+            size = hex(len(stripped_msg))[2:]
+            chunk = size + "\r\n" + stripped_msg + "\r\n"
+            #print "chunk: %s"%(chunk)
+            #conn.send(size)
             conn.send(chunk)
-            
     except Exception as e:
         print "some sort of problem", e
-
-    conn.send("0\r\n")
-    conn.close()
+    finally:
+        conn.close()
 
 def main():
     keys = swarmtoolscore.get_keys()
@@ -45,6 +59,9 @@ def main():
     swarm_id = sys.argv[1]
     resource_name = sys.argv[2]
     feed_name = sys.argv[3]
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     produce(keys["producer"], swarm_id, resource_name, feed_name)
 
 main()
