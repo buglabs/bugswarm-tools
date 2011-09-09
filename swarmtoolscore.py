f001032533f0c01e9dd4af9997803674a4017e77
barberdt <barberdt@gmail.com>
2011-09-09 19:24:47 -0400

    init now creates your keys for you if you odn't have any

--- swarmtoolscore.py
+++ swarmtoolscore.py
@@ -31,25 +31,52 @@ def set_user_info(user_id):
 def set_keys(user_id, password):
     config = ConfigParser.ConfigParser()
     config.read("%s/swarm.cfg"%(my_working_directory))
-    conn = httplib.HTTPConnection('api.bugswarm.net')
-    auth_hash = user_id + ":" + password
-    auth_header = "Basic " + base64.b64encode(auth_hash)
-    conn.request("GET", "/keys", None, {"Authorization":auth_header})
-    resp = conn.getresponse()
+    resp = get_keys_from_server(user_id, password)
+    if resp.status == 404:
+        print "Status is 404"
+        create_key(user_id, password, "master")
+        create_key(user_id, password, "producer")
+        create_key(user_id, password, "consumer")
+        resp = get_keys_from_server(user_id, password)
     txt = resp.read()
-    conn.close()
     json_obj = json.loads(txt)
     for key_obj in json_obj:
         key_type = key_obj["type"]
         key_value = key_obj["key"]
         config.set("Keys", key_type, key_value)
+
     if config.has_option("Keys", "master") == False:
-        config.set("Keys", "master", "none")
-    if config.has_option("Keys", "consumer") == False:
-        config.set("Keys", "consumer", "none")
+        key_obj = create_key(user_id, password, master)
+        config.set("Keys", "master", key_obj["key"])
     if config.has_option("Keys", "producer") == False:
-        config.set("Keys", "producer", "none")
+        key_obj = create_key(user_id, password, producer)
+        config.set("Keys", "producer", key_obj["key"])
+    if config.has_option("Keys", "consumer") == False:
+        key_obj = create_key(user_id, password, consumer)
+        config.set("Keys", "consumer", key_obj["key"])
 
     with open("swarm.cfg", "wb") as configfile:
         config.write(configfile)
 
+def create_key(user_id, password, key_type):
+    conn = httplib.HTTPConnection('api.bugswarm.net')
+    auth_hash = user_id + ":" + password
+    auth_header = "Basic " + base64.b64encode(auth_hash)
+    if (key_type != None):
+        conn.request("POST", "/keys/" + key_type, None, {"Authorization":auth_header})
+    else:
+        conn.request("POST", "/keys", None, {"Authorization":auth_header})
+    resp = conn.getresponse()
+    txt = resp.read()
+    conn.close()
+    json_obj = json.loads(txt)
+    return json_obj
+
+def get_keys_from_server(user_id, password):
+    conn = httplib.HTTPConnection('api.bugswarm.net')
+    auth_hash = user_id + ":" + password
+    auth_header = "Basic " + base64.b64encode(auth_hash)
+    conn.request("GET", "/keys", None, {"Authorization":auth_header})
+    resp = conn.getresponse()
+    conn.close()
+    return resp    
