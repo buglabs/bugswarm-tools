22605ed414b1bde1f892d07f0a5228102ecd36e6
Andrew Tergis <theterg@gmail.com>
2012-05-15 16:02:31 -0400

    participation example - simpletest.py - cleaner.

--- simpletest.py
+++ simpletest.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 from BUGswarm import apikey
 from BUGswarm import resource
-from BUGswarm import swarm
 from BUGswarm import participation
 import logging
-import time
-
-def presence(message):
-    print "presence from "+message['from']['resource']
 
 logging.basicConfig(level=logging.INFO)
-print "\n"
 api = apikey.apikey("demo","buglabs55")
 res = resource.getResourceByName(api,"Renesas01")
 swarms = res.getSwarms()
+
 print "Press enter to quit"
-pt = participation.participationThread(api,res, swarms, onPresence=presence)
+
+def presence(obj):
+    print "presence from "+obj['from']['resource']
+def message(obj):
+    print "message "+str(obj['payload'])
+def error(obj):
+    print "error "+str(obj['errors'])
+
+pt = participation.participationThread(api,res, swarms,
+        onPresence=presence, onMessage=message, onError=error)
 
 try:
     raw_input("")
